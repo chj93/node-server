413e17e25461b9a167547def3cceb31c0c27e629
chj93 <chj942@gmail.com>
2021-03-08 17:12:38 +0900

    admin 추가

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import router from "./router";
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser());
 app.get('/api/hello', (req, res) => {
   res.send('hello world');
 });
 
 app.use('/api', router);
+
 
 app.listen(8000, () => {
   console.log('server is listening 8000');
